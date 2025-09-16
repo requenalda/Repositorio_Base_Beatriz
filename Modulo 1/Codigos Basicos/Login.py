a7104351c1c266b079d1bce531f8e38a8c32d720
requenalda <requenabeatriz47@gmail.com>
2025-09-16 11:19:30 -0300

    Create Login.py

---
+++ Modulo 1/Codigos Basicos/Login.py
@@ -0,0 +1,13 @@
+nome_login = input("Digite seu no de login: ")
+senha_login = input("Digite sua senha para login: ")
+
+nome_cadastrado = input("Digite seu nome de cadastro: ")
+senha_cadastrada = input("Digite sua senha de cadastro: ")
+
+if nome_login == nome_cadastrado and senha_cadastrada == senha_login:
+    print("bem vindo")
+    
+else:
+    print("errado, tente novamente")
+while i <= 4322:
+    print(f" {i} x {numero} = {i * numero}")
